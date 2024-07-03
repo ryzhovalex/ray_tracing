ea6499a35b29d302b7188590b3f5548545d08eac
ryzhovalex <thed4rkof@gmail.com>
2024-07-03 03:50:56 +0300

    update imgui.ini, renderer.cpp

--- ray_tracing/src/renderer.cpp
+++ ray_tracing/src/renderer.cpp
@@ -37,6 +37,12 @@ void Renderer::render()
 				(float)x / (float)final_img->GetWidth(),
 				(float)y / (float)final_img->GetHeight()
 			};
+			coord = coord * 2.0f - 1.0f;
+			// remap coord from range (0,1) to range (-1,1) in order to
+			// to achieve anyside direction
+			// and yeah, coord here is our ray direction, where we
+			// additionally will define z coord which either 1 or -1 depending
+			// on camera forward (doesn't matter much for now)
 			img_data[x + y * final_img->GetWidth()] = per_pixel(coord);
 		}
 	}
@@ -47,6 +53,47 @@ uint32_t Renderer::per_pixel(glm::vec2 coord)
 {
 	auto r = (uint8_t)(coord.x * 255.0f);
 	auto g = (uint8_t)(coord.y * 255.0f);
+
+	// move camera back by 2.0f to be able to see the sphere in the middle,
+	// and not reside inside it
+	glm::vec3 ray_origin(0.0f, 0.0f, 2.0f);
+	glm::vec3 ray_dir(coord.x, coord.y, -1.0f);
+	glm::normalize(ray_dir);
+	float sphere_radius = 0.5f;
+
+	// quadratic equation at^2 + bt + c = 0
+	// (d_x^2 + d_y^2) * t^2 + (2 * (o_x * d_x + o_y * d_y)) * t + (o_x^2 + o_y^2 - r^2) = 0
+	// where
+	// o = ray origin
+	// d = ray direction
+	// r = radius
+	// t = hit distance (scaler)
+
+	// d_x^2 + d_y^2
+	//		= sqrt(d_x^2 + d_y^2) * sqrt(d_x^2 + d_y^2)
+	//		= ray_dir.magnitude * ray_dir.magnitude * cos(0)
+	//		= dot(ray_dir, ray_dir)
+	float a = glm::dot(ray_dir, ray_dir);
+	// 2 * (o_x * d_x + o_y * d_y)
+	//		= 2 * ray_origin.magnitude * ray_dir.magnitude * 1
+	//		= 2 * dot(ray_origin, ray_dir)
+	// DON'T UNDERSTAND THIS ENTIRELY
+	float b = 2.0f * glm::dot(ray_origin, ray_dir);
+	// same dot as for "a"
+	float c = glm::dot(ray_origin, ray_origin) - pow(sphere_radius, 2);
+
+	// d = b^2 - 4ac
+	float d = pow(b, 2) - 4.0f * a * c;
+	// if d >= 0, we're sure to have at least one solution, i.e. at least
+	// one intersection
+	// if we have at least one intersection -> we can draw sphere-coloured
+	// pixel
+	if (d >= 0.0f)
+	{
+		return 0xffff00ff;
+	}
+	return 0xff000000;
+
 	// rgba has 8 bits per color channel, so full rgba is 32 bits per pixel,
 	// in this case we use abgr (reversed)
 	// alpha=ff + blue = 00 + shifted left green + red
